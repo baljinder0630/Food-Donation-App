5ee8d0650a5db7824ab78ff679c4cd2c8cfd5f71
AnupamMittal-21 <anupammittal1234@gmail.com>
2024-01-15 02:03:04 +0530

    Improves GUI of Home page.

---
+++ lib/Pages/HomePages/hungerSpots.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class HungerSpot extends StatelessWidget {
+  final String child;
+
+  HungerSpot({required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(10),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: Colors.lightGreen, borderRadius: BorderRadius.circular(15)),
+      height: 320,
+      width: 300,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            child,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  "Hollywood Gali, Navrangpura Navrangpura",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      overflow: TextOverflow.ellipsis),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  "400m away",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 17,
+                      overflow: TextOverflow.ellipsis),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: 3,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.symmetric(horizontal: 5),
+                    child: CircleAvatar(
+                      radius: 50,
+                    ),
+                  );
+                }),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+            child: Text(
+              "View more",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/Pages/homePage.dart
+++ lib/Pages/homePage.dart
@@ -1,178 +1,158 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:food_donation_app/Pages/HomePages/hungerSpots.dart';
+import 'package:food_donation_app/Pages/HomePages/pickupRequest.dart';
 
-@RoutePage()
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  final List postId = [
+    '11',
+    '12',
+    '13',
+    '14',
+  ];
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
+  HomePage({Key? key}) : super(key: key);
 
-class _HomePageState extends State<HomePage> {
-
-  late String username;
-
-  void getData() async
-  {
-    print("In getData fun...");
-  }
-
-  void initState(){
-    super.initState();
-    getData();
-    print("In Init State..");
-  }
-
-  void setState(fn){
-    super.setState(() {
-      print("Int Set State... ");
-    });
-  }
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Column(
-          //  Column for greetings
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                CircleAvatar(radius: 40,),
-                Column(
-                  children: <Widget>[
-                    Text("Hello, ##Anupam##!"),
-                    Row(
-                      children: <Widget>[
-                        Text("##Location##"),
-                        Icon(Icons.place),
-                      ],
-                    )
-                  ],
-                )
-              ],
-            ),
-            SizedBox(height: 40),
-            Column(
-              children: <Widget>[
-                Text("Nearby Hunger Spots.. in ## Location ##."),
-                SizedBox(height: 20),
-                Row(
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Text("## Name of NGO ##"),
-                        Text("## 400m ##"),
-                        CircleAvatar(radius: 30,)
-                      ],
-                    ),
-                    SizedBox(width: 20,),
-                    Column(
-                      children: <Widget>[
-                        Text("## Name of NGO2 ##"),
-                        Text("## 900m ##"),
-                        CircleAvatar(radius: 30,)
-                      ],
-                    ),
-                    SizedBox(width: 20,),
-                    Column(
-                      children: <Widget>[
-                        Text("## Name of NGO3 ##"),
-                        Text("## 1300m ##"),
-                        CircleAvatar(radius: 30,)
-                      ],
-                    )
-                  ],
-                )
-
-              ],
-            ),
-            SizedBox(height: 50),
-            Column(
-              children: <Widget>[
-                Text("Food Pickup request in ## Location ##."),
-                SizedBox(height: 30),
-                Row(
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Text("## Name Reciepe1 ##"),
-                        Text("## Cooked ##4m## ago ##"),
-                        Row(children: [CircleAvatar(radius: 30,),SizedBox(width: 4,), CircleAvatar(radius: 30,),],),
-                        Row(children: [Icon(Icons.place),Text("##Address##")],),
-                        Text("##2 KM## away(approx)."),
-                        Row(
+        child: ListView.builder(
+            itemCount: 1,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 50,
+                        ),
+                        SizedBox(width: 20),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: <Widget>[
-                            ElevatedButton(
-                              onPressed: (){},
-                              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-                              child: Text("Accept"),
+                            Row(children: [
+                              Text(
+                                "Hello, Anupam! ",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    overflow: TextOverflow.ellipsis),
+                              ),
+                            ]),
+                            Row(
+                              children: [
+                                Icon(Icons.place),
+                                Text(
+                                  "Meerut.",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 17,
+                                      overflow: TextOverflow.ellipsis),
+                                ),
+                              ],
                             ),
-                            ElevatedButton(
-                              onPressed: (){},
-                              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-                              child: Text("Reject"),
-                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    padding: EdgeInsets.all(10),
+                    height: 80,
+                    decoration: BoxDecoration(
+                        color: Colors.lightGreen,
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Nearby hunger spots you can serve in ",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                              overflow: TextOverflow.ellipsis),
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.place),
+                            Expanded(
+                              child: Text(
+                                "Gulbai Tekra, Navrangpura.",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                    overflow: TextOverflow.ellipsis),
+                              ),
+                            ),
                           ],
                         ),
                       ],
                     ),
-                    SizedBox(width: 20,),
-                    Column(
-                      children: <Widget>[
-                        Text("## Name Reciepe2 ##"),
-                        Text("## Cooked ##4m## ago ##"),
-                        CircleAvatar(radius: 30,),
-                        Row(children: [Icon(Icons.place),Text("##Address##")],),
-                        Text("##2 KM## away(approx)."),
+                  ),
+                  Container(
+                    height: 320,
+                    child: ListView.builder(
+                        itemCount: postId.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return HungerSpot(child: postId[index]);
+                        }),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    padding: EdgeInsets.all(10),
+                    height: 80,
+                    decoration: BoxDecoration(
+                        color: Colors.lightGreen,
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Food Pickup requests you can serve from",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                              overflow: TextOverflow.ellipsis),
+                        ),
                         Row(
-                          children: <Widget>[
-                            ElevatedButton(
-                              onPressed: (){},
-                              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-                              child: Text("Accept"),
+                          children: [
+                            Icon(Icons.place),
+                            Expanded(
+                              child: Text(
+                                "Gulbai Tekra, Navrangpura.",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                    overflow: TextOverflow.ellipsis),
+                              ),
                             ),
-                            ElevatedButton(
-                              onPressed: (){},
-                              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-                              child: Text("Reject"),
-                            )
                           ],
                         ),
                       ],
                     ),
-                    SizedBox(width: 20,),
-                    Column(
-                      children: <Widget>[
-                        Text("## Name Reciepe3 ##"),
-                        Text("## Cooked ##4m## ago ##"),
-                        CircleAvatar(radius: 30,),
-                        Row(children: [Icon(Icons.place),Text("##Address##")],),
-                        Text("##2 KM## away(approx)."),
-                        Row(
-                          children: <Widget>[
-                            ElevatedButton(
-                              onPressed: (){},
-                              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-                              child: Text("Accept"),
-                            ),
-                            ElevatedButton(
-                              onPressed: (){},
-                              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-                              child: Text("Reject"),
-                            )
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                )
-
-              ],
-            ),
-            Text("HH")
-          ],
-        ),
+                  ),
+                  Container(
+                    height: 350,
+                    child: ListView.builder(
+                        itemCount: postId.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return PickUpReequest();
+                        }),
+                  ),
+                ],
+              );
+            }),
       ),
     );
   }
