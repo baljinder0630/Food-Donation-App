e012b209c3d8e3ada06b5ffe979ef4069000dec3
AnupamMittal-21 <anupammittal1234@gmail.com>
2024-01-14 17:03:23 +0530

    Adding Home page

--- lib/Pages/homePage.dart
+++ lib/Pages/homePage.dart
@@ -3,18 +3,177 @@ import 'package:flutter/material.dart';
 
 @RoutePage()
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+
+  late String username;
+
+  void getData() async
+  {
+    print("In getData fun...");
+  }
+
+  void initState(){
+    super.initState();
+    getData();
+    print("In Init State..");
+  }
+
+  void setState(fn){
+    super.setState(() {
+      print("Int Set State... ");
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: const Center(
-      child: Text("Home Page"),
-    ));
+    return  Scaffold(
+      body: SafeArea(
+        child: Column(
+          //  Column for greetings
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                CircleAvatar(radius: 40,),
+                Column(
+                  children: <Widget>[
+                    Text("Hello, ##Anupam##!"),
+                    Row(
+                      children: <Widget>[
+                        Text("##Location##"),
+                        Icon(Icons.place),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+            SizedBox(height: 40),
+            Column(
+              children: <Widget>[
+                Text("Nearby Hunger Spots.. in ## Location ##."),
+                SizedBox(height: 20),
+                Row(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Text("## Name of NGO ##"),
+                        Text("## 400m ##"),
+                        CircleAvatar(radius: 30,)
+                      ],
+                    ),
+                    SizedBox(width: 20,),
+                    Column(
+                      children: <Widget>[
+                        Text("## Name of NGO2 ##"),
+                        Text("## 900m ##"),
+                        CircleAvatar(radius: 30,)
+                      ],
+                    ),
+                    SizedBox(width: 20,),
+                    Column(
+                      children: <Widget>[
+                        Text("## Name of NGO3 ##"),
+                        Text("## 1300m ##"),
+                        CircleAvatar(radius: 30,)
+                      ],
+                    )
+                  ],
+                )
+
+              ],
+            ),
+            SizedBox(height: 50),
+            Column(
+              children: <Widget>[
+                Text("Food Pickup request in ## Location ##."),
+                SizedBox(height: 30),
+                Row(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Text("## Name Reciepe1 ##"),
+                        Text("## Cooked ##4m## ago ##"),
+                        Row(children: [CircleAvatar(radius: 30,),SizedBox(width: 4,), CircleAvatar(radius: 30,),],),
+                        Row(children: [Icon(Icons.place),Text("##Address##")],),
+                        Text("##2 KM## away(approx)."),
+                        Row(
+                          children: <Widget>[
+                            ElevatedButton(
+                              onPressed: (){},
+                              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                              child: Text("Accept"),
+                            ),
+                            ElevatedButton(
+                              onPressed: (){},
+                              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                              child: Text("Reject"),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 20,),
+                    Column(
+                      children: <Widget>[
+                        Text("## Name Reciepe2 ##"),
+                        Text("## Cooked ##4m## ago ##"),
+                        CircleAvatar(radius: 30,),
+                        Row(children: [Icon(Icons.place),Text("##Address##")],),
+                        Text("##2 KM## away(approx)."),
+                        Row(
+                          children: <Widget>[
+                            ElevatedButton(
+                              onPressed: (){},
+                              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                              child: Text("Accept"),
+                            ),
+                            ElevatedButton(
+                              onPressed: (){},
+                              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                              child: Text("Reject"),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 20,),
+                    Column(
+                      children: <Widget>[
+                        Text("## Name Reciepe3 ##"),
+                        Text("## Cooked ##4m## ago ##"),
+                        CircleAvatar(radius: 30,),
+                        Row(children: [Icon(Icons.place),Text("##Address##")],),
+                        Text("##2 KM## away(approx)."),
+                        Row(
+                          children: <Widget>[
+                            ElevatedButton(
+                              onPressed: (){},
+                              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                              child: Text("Accept"),
+                            ),
+                            ElevatedButton(
+                              onPressed: (){},
+                              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                              child: Text("Reject"),
+                            )
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                )
+
+              ],
+            ),
+            Text("HH")
+          ],
+        ),
+      ),
+    );
   }
 }
