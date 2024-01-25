b4d8b2d56b909b9cc4a8dcec79e3c3350a56232b
Baljinder singh <baljindersingh41265@gmail.com>
2024-01-26 00:27:56 +0530

    Added Shimmer effect

--- lib/Pages/SplashScreen/splashScreen.dart
+++ lib/Pages/SplashScreen/splashScreen.dart
@@ -17,7 +17,8 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
   Widget build(BuildContext context) {
     ref.listen(authStateProvider, (previous, next) {
       if (next.appStatus == AppStatus.authenticated) {
-        context.router.replace(const AppBottomNavigationBarRoute());
+        // context.router.replace(const AppBottomNavigationBarRoute());
+        context.router.replace(const CommunityHomePageRoute());
       } else {
         context.router.replace(const SignUpPageRoute());
       }
