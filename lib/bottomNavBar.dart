6858ab3f19011a27fcbcecf3c1cb00633bea2c13
AnupamMittal-21 <anupammittal1234@gmail.com>
2024-02-22 02:22:11 +0530

    added nav bar .

--- lib/bottomNavBar.dart
+++ lib/bottomNavBar.dart
@@ -1,76 +1,81 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:circle_nav_bar/circle_nav_bar.dart';
 import 'package:food_donation_app/Pages/Community/communityScreen.dart';
 import 'package:food_donation_app/Pages/DashBoard/dashBoard.dart';
-import 'package:food_donation_app/Pages/Donate/personal_details_form.dart';
 import 'package:food_donation_app/Pages/Posts/PostsHomePage.dart';
 import 'package:food_donation_app/Pages/homePage.dart';
-import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:food_donation_app/Pages/Donationland/landdonation.dart';
+// Import other necessary packages
 
-import 'Pages/constants/constants.dart';
+// Import other necessary packages
 
-@RoutePage()
 class AppBottomNavigationBar extends StatefulWidget {
   @override
   _AppBottomNavigationBarState createState() => _AppBottomNavigationBarState();
 }
 
 class _AppBottomNavigationBarState extends State<AppBottomNavigationBar> {
-  int _currentIndex = 0;
+  late PageController pageController;
+  int tabIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   final _pages = [
     HomePage(),
     PostHomePage(),
-    // const DonationRequest(),
-    const landDonation(),
-    // PersonalDetails(),
-    const CommunityHomePage(),
-    const DashBoardPage(),
-    // const ProfilePage(),
+    landDonation(),
+    CommunityHomePage(),
+    DashBoardPage(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_currentIndex],
-      bottomNavigationBar: PersistentTabView(
-        context,
-        controller: PersistentTabController(initialIndex: _currentIndex),
-        screens: _pages,
-        items: [
-          PersistentBottomNavBarItem(
-            icon: const Icon(Icons.home),
-            title: 'Home',
-            activeColorPrimary: green,
-            inactiveColorPrimary: Colors.grey,
-          ),
-          PersistentBottomNavBarItem(
-            icon: const Icon(Icons.comment),
-            title: 'Donate Now',
-            activeColorPrimary: green,
-            inactiveColorPrimary: Colors.grey,
-          ),
-          PersistentBottomNavBarItem(
-            icon: const Icon(Icons.add_alarm),
-            title: 'Donate Now',
-            activeColorPrimary: green,
-            inactiveColorPrimary: Colors.grey,
-          ),
-          PersistentBottomNavBarItem(
-            icon: const Icon(Icons.group),
-            title: 'Community',
-            activeColorPrimary: green,
-            inactiveColorPrimary: Colors.grey,
-          ),
-          PersistentBottomNavBarItem(
-            icon: const Icon(Icons.dashboard),
-            title: 'Dashboard',
-            activeColorPrimary: green,
-            inactiveColorPrimary: Colors.grey,
-          ),
+      body: PageView(
+        controller: pageController,
+        onPageChanged: (index) {
+          setState(() => tabIndex = index);
+        },
+        children: _pages,
+      ),
+      bottomNavigationBar: CircleNavBar(
+        activeIcons: const [
+          Icon(Icons.home, color: Colors.green),
+          Icon(Icons.chat_bubble, color: Colors.green),
+          Icon(Icons.add, color: Colors.green),
+          Icon(Icons.group, color: Colors.green),
+          Icon(Icons.account_circle, color: Colors.green),
         ],
-        // ... rest of the configuration remains the same
+        inactiveIcons: const [
+          Text("Home"),
+          Text("Chat"),
+          Text("Donate"),
+          Text("Community"),
+          Text("Profile"),
+        ],
+        color: Colors.white,
+        circleColor: Colors.white,
+        height: 60,
+        circleWidth: 60,
+        activeIndex: tabIndex,
+        onTap: (index) {
+          // Assuming onTap is the correct method to use
+          setState(() {
+            tabIndex = index;
+          });
+          pageController.jumpToPage(index);
+        },
+        // Other properties as before
       ),
     );
   }
